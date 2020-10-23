be06546fbd8acd8f4c27890387f54d22f944428f
Alexander Zellner <alexander.zellner@tum.de>
2020-10-23 12:57:56 +0200

    ask user if iTerm will be used

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# DISCLAIMER execute this in
+while true; do
+    read -p "Are you currently using iTerm2 (y(es)/n(o)?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * );;
+    esac
+done

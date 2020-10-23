b89a72be21d03182123fa4628d55385113faf1ad
Alexander Zellner <alexander.zellner@tum.de>
2020-10-23 12:58:57 +0200

    setup vim

--- setup.sh
+++ setup.sh
@@ -26,3 +26,8 @@ brew cask install fork
 git config --global user.name "Alexander Zellner"
 git config --global user.email "alexander.zellner@tum.de"
 echo ".DS_Store" >> ~/.gitignore
+
+# Vim setup
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo "set number relativenumber" >> ~/.vimrc
